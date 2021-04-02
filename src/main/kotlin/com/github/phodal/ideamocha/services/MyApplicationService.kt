d866c65b73e9695607ff4a85ab48aa23ff4fb8e1
GitHub Action <action@github.com>
2021-04-02 13:10:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/phodal/ideamocha/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/phodal/ideamocha/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.phodal.ideamocha.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.phodal.ideamocha.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/phodal/ideamocha/services/MyProjectService.kt
+++ src/main/kotlin/com/github/phodal/ideamocha/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.phodal.ideamocha.services
 
+import com.github.phodal.ideamocha.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
